64fd26b2376c47f6eb8ccc3540dbb87e4f1b9c8e
Clear008 <wissal-exo@hotmail.com>
2023-12-15 02:00:42 +0100

    still trying

--- function_helper.c
+++ function_helper.c
@@ -21,14 +21,14 @@ int w_exet(char *content, stack_t **stack, unsigned int line_number, FILE *file)
 	if (w_op && w_op[0] == '#')
 		return (0);
 	gv.arg = strtok(NULL, " $\n\t");
-	for (i = 0; wopc[i].opcode; i++)
-    {
-        if (strcmp(w_op, wopc[i].opcode) == 0)
-        {
-            wopc[i].f(stack, line_number);
-            return 0;
-        }
-    }
+for (i = 0; wopc[i].opcode && w_op; i++)
+{
+if (strcmp(w_op, wopc[i].opcode) == 0)
+{
+wopc[i].f(stack, line_number);
+return (0);
+}
+}
 
 	if (w_op && wopc[i].opcode == NULL)
 	{ fprintf(stderr, "L%d: unknown instruction %s\n", line_number, w_op);
